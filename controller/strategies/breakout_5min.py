93744f7506046db4aab69a643c75213185c895e1
ramkumarrammohan <ramkumarrammohan@gmail.com>
2019-08-04 20:06:58 +0530

    breakout strategy analysis calc changed

--- controller/strategies/breakout_5min.py
+++ controller/strategies/breakout_5min.py
@@ -4,19 +4,31 @@ from models.script import Script
 from models.ticker_5min import Ticker5min
 from models.strategy_breakout_5min import StrategyBreakout_5Min
 from utils.pretty_json import *
+from db.database import atomic_insert
+from datetime import datetime
+from peewee import fn
 
 # global members'
 ScriptIdKey = 'script_id'
 DateKey = 'date'
 UpperCutOffPriceKey = 'upper_cutoff_price'
 LowerCutOffPriceKey = 'lower_cutoff_price'
-BuyKey = 'buy'
+DayMaxPriceKey = 'day_max_price'
+DayMinPriceKey = 'day_min_price'
+
+SellEntryTimeKey = 'sell_entry_time'
+SellProfitOnePercentTimeKey = 'sell_profit_one_percent_time'
+SellMaxStoplossBeforeOnePercentKey = 'sell_max_stoploss_before_one_percent'
+
 BuyEntryTimeKey = 'buy_entry_time'
+BuyProfitOnePercentTimeKey = 'buy_profit_one_percent_time'
+BuyMaxStoplossBeforeOnePercentKey = 'buy_max_stoploss_before_one_percent'
+
 MaxPosAfterBuyEntryKey = 'max_positive_after_buy_entry'
 MaxNegAfterBuyEntryKey = 'max_negative_after_buy_entry'
 TimeReachOnePercentAfterBuyKey = 'time_to_reach_one_percent_after_buy'
 SellKey = 'sell'
-SellEntryTimeKey = 'sell_entry_time'
+
 MaxPosAfterSellEntryKey = 'max_positive_after_sell_entry'
 MaxNegAfterSellEntryKey = 'max_negative_after_sell_entry'
 TimeReachOnePercentAfterSellKey = 'time_to_reach_one_percent_after_sell'
@@ -24,17 +36,14 @@ TimeReachOnePercentAfterSellKey = 'time_to_reach_one_percent_after_sell'
 
 class Breakout5min():
     def __init__(self):
-        print("breakout analyser")
+        print("Breakout analyzer added")
 
     def apply(self):
+        start_time = datetime.now()
         if not StrategyBreakout_5Min.table_exists():
-            print('StrategyBreakout_5Min table not found')
             StrategyBreakout_5Min.create_table()
-            print('StrategyBreakout_5Min created')
 
         # get all scripts
-        # scripts = Script.select(Script.id, Script.symbol,
-                                # Script.company_name).where(Script.symbol == 'TATAMOTORS')
         scripts = Script.select(Script.id, Script.symbol,
                                 Script.company_name).where(Script.alpha_support == True)
         # Iterate through scripts and backtest the strategy for each script
@@ -46,8 +55,9 @@ class Breakout5min():
                 StrategyBreakout_5Min.script_id == script.id).order_by(
                 StrategyBreakout_5Min.date.desc()).limit(1))
             if analysis_start_date:
-                # replace analysis_start_date with date in 'DATE_FORMAT'
-                print('Records found for the script: {}'.format(script.symbol))
+                analysis_start_date = analysis_start_date[0].date + timedelta(
+                    days=1)
+                print('starting analysis from: '+str(analysis_start_date))
             else:
                 analysis_start_date = (Ticker5min.select().where(
                     Ticker5min.script_id == script.id).order_by(
@@ -60,6 +70,8 @@ class Breakout5min():
                     continue
             self.analyze(script.id, analysis_start_date.strftime(
                 DATE_FORMAT) + ' 00:00:00')
+        end_time = datetime.now()
+        print('elapsed time: ' + str(end_time - start_time))
 
     def analyze(self, script_id, from_date):
         date_time_obj = datetime.strptime(from_date, DATE_TIME_FORMAT)
@@ -71,103 +83,145 @@ class Breakout5min():
             if result_dict:
                 analysis_list.append(result_dict)
             date_time_obj = date_time_obj + timedelta(days=1)
-        print_json(analysis_list)
+        # print_json(analysis_list)
+        atomic_insert(StrategyBreakout_5Min, analysis_list)
 
     def get_one_trade_day_analysis(self, script_id, dt_obj):
         # weekend check
         if not is_weekday(dt_obj):
             return None
 
-        filter_start_time = dt_obj
-        filter_end_time = dt_obj + timedelta(hours=23, minutes=59, seconds=59)
-        ticker_data = (Ticker5min.select()
-                       .where(Ticker5min.script_id == script_id,
-                              Ticker5min.record_datetime >= filter_start_time,
-                              Ticker5min.record_datetime < filter_end_time)
-                       .order_by(Ticker5min.record_datetime.asc()))
-        if ticker_data:
-            expected_start_time = dt_obj + timedelta(hours=9, minutes=15)
-            expected_end_time = dt_obj + timedelta(hours=15, minutes=25)
-            record_time_start = ticker_data[0].record_datetime
-            record_time_end = ticker_data[len(ticker_data)-1].record_datetime
-            if (record_time_start != expected_start_time or record_time_end != expected_end_time):
-                print('Partial data found for the date: {}'
-                      .format(dt_obj.strftime(DATE_FORMAT)))
-                return None
+        first_candle_time = dt_obj.replace(hour=9, minute=15)
+        last_candle_time = dt_obj.replace(hour=15, minute=25)
+        ticker_count_validness = (Ticker5min
+                                  .select()
+                                  .where(
+                                      Ticker5min.script_id == script_id,
+                                      Ticker5min.record_datetime.in_(
+                                          [last_candle_time, first_candle_time]))
+                                  .count()
+                                  )
 
-            observation_time_end = dt_obj + timedelta(hours=10)
-            result_data = {}
-            result_data[ScriptIdKey] = script_id
-            result_data[DateKey] = dt_obj.strftime(DATE_FORMAT)
-            result_data[UpperCutOffPriceKey] = 0.0
-            result_data[LowerCutOffPriceKey] = 0.0
-            result_data[MaxPosAfterBuyEntryKey] = 0.0
-            result_data[MaxNegAfterBuyEntryKey] = 0.0
-            result_data[MaxPosAfterSellEntryKey] = 0.0
-            result_data[MaxNegAfterSellEntryKey] = 0.0
-            result_data[BuyKey] = False
-            result_data[SellKey] = False
-            result_data[BuyEntryTimeKey] = None
-            result_data[SellEntryTimeKey] = None
-            result_data[TimeReachOnePercentAfterBuyKey] = None
-            result_data[TimeReachOnePercentAfterSellKey] = None
-            onePercentProfitBuyPrice = 0.0
-            onePercentProfitSellPrice = 0.0
+        if ticker_count_validness == 2:
+            result_dict = {}
+            result_dict[DateKey] = dt_obj
+            result_dict[ScriptIdKey] = script_id
+            # query day low and high prices
+            market_opening_time = dt_obj.replace(hour=9, minute=15, second=00)
+            market_closing_time = dt_obj.replace(hour=15, minute=30, second=00)
+            day_low_high = (Ticker5min
+                            .select(fn.Min(Ticker5min.low_price), fn.Max(Ticker5min.high_price))
+                            .where(Ticker5min.script_id == script_id,
+                                   Ticker5min.record_datetime >= market_opening_time,
+                                   Ticker5min.record_datetime <= market_closing_time)
+                            .scalar(as_tuple=True)
+                            )
+            result_dict[DayMinPriceKey] = round(day_low_high[0], 2)
+            result_dict[DayMaxPriceKey] = round(day_low_high[1], 2)
+            # query cutoff price for day
+            one_hr_start = dt_obj.replace(hour=9, minute=15, second=00)
+            one_hr_end = dt_obj.replace(hour=10, minute=00, second=00)
+            cut_off_prices = (Ticker5min
+                              .select(fn.Min(Ticker5min.low_price), fn.Max(Ticker5min.high_price))
+                              .where(Ticker5min.script_id == script_id,
+                                     Ticker5min.record_datetime >= one_hr_start,
+                                     Ticker5min.record_datetime <= one_hr_end)
+                              .scalar(as_tuple=True)
+                              )
+            result_dict[LowerCutOffPriceKey] = round(cut_off_prices[0], 2)
+            result_dict[UpperCutOffPriceKey] = round(cut_off_prices[1], 2)
 
-            for ticker in ticker_data:
-                if (ticker.record_datetime < observation_time_end):
-                    if (result_data[LowerCutOffPriceKey] == 0.0):
-                        result_data[LowerCutOffPriceKey] = ticker.low_price
-                    else:
-                        result_data[LowerCutOffPriceKey] = ((lambda: result_data[LowerCutOffPriceKey],
-                                                             lambda: ticker.low_price)
-                                                            [ticker.low_price < result_data[LowerCutOffPriceKey]]())
-                        onePercentProfitSellPrice = (result_data[LowerCutOffPriceKey] +
-                                                     result_data[LowerCutOffPriceKey] / 100)
-                    if result_data[UpperCutOffPriceKey] == 0.0:
-                        result_data[UpperCutOffPriceKey] = ticker.high_price
-                    else:
-                        result_data[UpperCutOffPriceKey] = ((lambda: result_data[UpperCutOffPriceKey],
-                                                             lambda: ticker.high_price)
-                                                            [ticker.high_price > result_data[UpperCutOffPriceKey]]())
-                        onePercentProfitBuyPrice = (result_data[UpperCutOffPriceKey] +
-                                                    result_data[UpperCutOffPriceKey] / 100)
+            # query sell entry time if any
+            sell_entry = (Ticker5min
+                          .select(Ticker5min.record_datetime)
+                          .where(Ticker5min.script_id == script_id,
+                                 Ticker5min.low_price < result_dict[LowerCutOffPriceKey],
+                                 Ticker5min.record_datetime > one_hr_end,
+                                 Ticker5min.record_datetime <= market_closing_time)
+                          .order_by(Ticker5min.record_datetime.asc())
+                          .limit(1)
+                          )
+            if sell_entry:
+                result_dict[SellEntryTimeKey] = sell_entry[0].record_datetime
+                # query for remaining column for sell side
+                one_percent_lower_cutoff = (result_dict[LowerCutOffPriceKey] -
+                                            result_dict[LowerCutOffPriceKey] * 0.01)
+                one_percent_sell_profit = (Ticker5min
+                                           .select(Ticker5min.record_datetime)
+                                           .where(Ticker5min.script_id == script_id,
+                                                  Ticker5min.low_price < one_percent_lower_cutoff,
+                                                  Ticker5min.record_datetime >= result_dict[SellEntryTimeKey],
+                                                  Ticker5min.record_datetime <= market_closing_time)
+                                           .order_by(Ticker5min.record_datetime.asc())
+                                           .limit(1)
+                                           )
+                if one_percent_sell_profit:
+                    result_dict[SellProfitOnePercentTimeKey] = one_percent_sell_profit[0].record_datetime
+                    sell_max_stop_loss = (Ticker5min
+                                          .select(fn.Max(Ticker5min.high_price))
+                                          .where(Ticker5min.script_id == script_id,
+                                                 Ticker5min.record_datetime >= result_dict[SellEntryTimeKey],
+                                                 Ticker5min.record_datetime <= result_dict[SellProfitOnePercentTimeKey])
+                                          .scalar()
+                                          )
+                    sell_max_stop_loss_percent = ((sell_max_stop_loss - result_dict[LowerCutOffPriceKey]) /
+                                                  result_dict[LowerCutOffPriceKey]) * 100
+                    result_dict[SellMaxStoplossBeforeOnePercentKey] = round(
+                        sell_max_stop_loss_percent, 2)
                 else:
-                    if (ticker.high_price > result_data[UpperCutOffPriceKey]) and result_data[BuyKey] == False:
-                        result_data[BuyKey] = True
-                        result_data[BuyEntryTimeKey] = (ticker.record_datetime
-                                                        .strftime(TIME_FORMAT))
-                        result_data[MaxPosAfterBuyEntryKey] = ticker.high_price
-                        result_data[MaxNegAfterBuyEntryKey] = ticker.low_price
-                    else:
-                        result_data[MaxPosAfterBuyEntryKey] = ((lambda: result_data[MaxPosAfterBuyEntryKey],
-                                                                lambda: ticker.high_price)
-                                                               [ticker.high_price > result_data[MaxPosAfterBuyEntryKey]]())
-                        result_data[MaxNegAfterBuyEntryKey] = ((lambda: result_data[MaxNegAfterBuyEntryKey],
-                                                                lambda: ticker.low_price)
-                                                               [ticker.low_price < result_data[MaxNegAfterBuyEntryKey]]())
-                        if (result_data[TimeReachOnePercentAfterBuyKey] == None and
-                                result_data[MaxPosAfterBuyEntryKey] > onePercentProfitBuyPrice):
-                            result_data[TimeReachOnePercentAfterBuyKey] = (ticker.record_datetime
-                                                                           .strftime(TIME_FORMAT))
-                    if (ticker.low_price < result_data[LowerCutOffPriceKey]) and result_data[SellKey] == False:
-                        result_data[SellKey] = True
-                        result_data[SellEntryTimeKey] = (ticker.record_datetime
-                                                         .strftime(TIME_FORMAT))
-                        result_data[MaxPosAfterSellEntryKey] = ticker.low_price
-                        result_data[MaxNegAfterSellEntryKey] = ticker.high_price
-                    else:
-                        result_data[MaxPosAfterSellEntryKey] = ((lambda: result_data[MaxPosAfterSellEntryKey],
-                                                                 lambda: ticker.low_price)
-                                                                [ticker.low_price < result_data[MaxPosAfterSellEntryKey]]())
-                        result_data[MaxNegAfterSellEntryKey] = ((lambda: result_data[MaxNegAfterSellEntryKey],
-                                                                 lambda: ticker.high_price)
-                                                                [ticker.high_price > result_data[MaxNegAfterSellEntryKey]]())
-                        if (result_data[TimeReachOnePercentAfterSellKey] == None and
-                                result_data[MaxPosAfterSellEntryKey] > onePercentProfitSellPrice):
-                            result_data[TimeReachOnePercentAfterSellKey] = (ticker.record_datetime
-                                                                            .strftime(TIME_FORMAT))
-            return result_data
+                    result_dict[SellProfitOnePercentTimeKey] = None
+                    result_dict[SellMaxStoplossBeforeOnePercentKey] = None
+            else:
+                result_dict[SellEntryTimeKey] = None
+                result_dict[SellProfitOnePercentTimeKey] = None
+                result_dict[SellMaxStoplossBeforeOnePercentKey] = None
+
+            # query buy entry time if any
+            buy_entry = (Ticker5min
+                         .select(Ticker5min.record_datetime)
+                         .where(Ticker5min.script_id == script_id,
+                                Ticker5min.high_price > result_dict[UpperCutOffPriceKey],
+                                Ticker5min.record_datetime > one_hr_end,
+                                Ticker5min.record_datetime <= market_closing_time)
+                         .order_by(Ticker5min.record_datetime.asc())
+                         .limit(1)
+                         )
+            if buy_entry:
+                result_dict[BuyEntryTimeKey] = buy_entry[0].record_datetime
+                # query for remaining column for buy side
+                one_percent_upper_cutoff = (result_dict[UpperCutOffPriceKey] +
+                                            result_dict[UpperCutOffPriceKey] * 0.01)
+                one_percent_buy_profit = (Ticker5min
+                                          .select(Ticker5min.record_datetime)
+                                          .where(Ticker5min.script_id == script_id,
+                                                 Ticker5min.high_price > one_percent_upper_cutoff,
+                                                 Ticker5min.record_datetime >= result_dict[BuyEntryTimeKey],
+                                                 Ticker5min.record_datetime <= market_closing_time,)
+                                          .order_by(Ticker5min.record_datetime.asc())
+                                          .limit(1)
+                                          )
+                if one_percent_buy_profit:
+                    result_dict[BuyProfitOnePercentTimeKey] = one_percent_buy_profit[0].record_datetime
+                    buy_max_stop_loss = (Ticker5min
+                                         .select(fn.Min(Ticker5min.low_price))
+                                         .where(Ticker5min.script_id == script_id,
+                                                Ticker5min.record_datetime >= result_dict[BuyEntryTimeKey],
+                                                Ticker5min.record_datetime <= result_dict[BuyProfitOnePercentTimeKey])
+                                         .scalar()
+                                         )
+                    buy_max_stop_loss_percent = ((result_dict[UpperCutOffPriceKey] - buy_max_stop_loss) /
+                                                 result_dict[UpperCutOffPriceKey]) * 100
+                    result_dict[BuyMaxStoplossBeforeOnePercentKey] = round(
+                        buy_max_stop_loss_percent, 2)
+                else:
+                    result_dict[BuyProfitOnePercentTimeKey] = None
+                    result_dict[BuyMaxStoplossBeforeOnePercentKey] = None
+            else:
+                result_dict[BuyEntryTimeKey] = None
+                result_dict[BuyProfitOnePercentTimeKey] = None
+                result_dict[BuyMaxStoplossBeforeOnePercentKey] = None
+
+            return result_dict
 
         else:
             return None
--- models/strategy_breakout_5min.py
+++ models/strategy_breakout_5min.py
@@ -7,23 +7,24 @@ from db.database import BaseModel
 class StrategyBreakout_5Min(BaseModel):
     created_at = peewee.DateTimeField(default=datetime.datetime.now)
     updated_at = peewee.DateTimeField(default=datetime.datetime.now)
-    script_id = peewee.ForeignKeyField(Script, backref='script', unique=True)
+    # reference field
+    script_id = peewee.ForeignKeyField(Script, backref='script')
+    # trading date
     date = peewee.DateField()
+    # day max values
+    day_max_price = peewee.FloatField()
+    day_min_price = peewee.FloatField()
     # entry criteria
     upper_cutoff_price = peewee.FloatField()
     lower_cutoff_price = peewee.FloatField()
     # buy prop
-    buy = peewee.BooleanField()
     buy_entry_time = peewee.TimeField(null=True)
-    max_positive_after_buy_entry = peewee.FloatField()
-    max_negative_after_buy_entry = peewee.FloatField()
-    time_to_reach_one_percent_after_buy = peewee.TimeField(null=True)
+    buy_profit_one_percent_time = peewee.TimeField(null=True)
+    buy_max_stoploss_before_one_percent = peewee.FloatField(null=True)
     # sell prop
-    sell = peewee.BooleanField()
-    sell_entry_time = peewee.DateTimeField(null=True)
-    max_positive_after_sell_entry = peewee.FloatField()
-    max_negative_after_sell_entry = peewee.FloatField()
-    time_to_reach_one_percent_after_sell = peewee.TimeField(null=True)
+    sell_entry_time = peewee.TimeField(null=True)
+    sell_profit_one_percent_time = peewee.TimeField(null=True)
+    sell_max_stoploss_before_one_percent = peewee.FloatField(null=True)
 
     def save(self, *args, **kwargs):
         self.updated_at = datetime.datetime.now()
